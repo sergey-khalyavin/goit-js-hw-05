488801dc4781f118c094228e818e1e0e8ea8e4c9
Sergey Khalyavin <serg.khalk@gmail.com>
2020-05-23 15:25:24 +0300

    Add files of fifth HW-JS

---
+++ js/task-3.js
@@ -0,0 +1,40 @@
+'use strict';
+/*Напиши класс Storage, который будет создавать объекты 
+для управления складом товаров. При вызове будет 
+получать один аргумент - начальный массив товаров, 
+и записывать его в свойство items.*/
+
+class Storage {
+  constructor(products) {
+    this.items = products;
+  }
+
+  getItems() {
+    return this.items;
+  }
+
+  addItem(item) {
+    this.items.push(item);
+  }
+
+  removeItem(item) {
+    let idx = this.items.indexOf(item);
+    this.items.splice(idx, 1);
+  }
+}
+
+const storage = new Storage([
+  'Нанитоиды',
+  'Пролонгер',
+  'Железные жупи',
+  'Антигравитатор',
+]);
+
+const items = storage.getItems();
+console.table(items); // [ "Нанитоиды", "Пролонгер", "Железные жупи", "Антигравитатор" ]
+
+storage.addItem('Дроид');
+console.table(storage.items); // [ "Нанитоиды", "Пролонгер", "Железные жупи", "Антигравитатор", "Дроид" ]
+
+storage.removeItem('Пролонгер');
+console.table(storage.items); // [ "Нанитоиды", "Железные жупи", "Антигравитатор", "Дроид" ]
